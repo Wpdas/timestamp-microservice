dc2e0969677c77c76bd18e508130e889d8a5bbbb
Wenderson Pires <wpdas@yahoo.com.br>
2019-05-16 03:42:43 -0300

    Adjustments

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Project: APIs and Microservices Projects - Timestamp Microservice
 ### Important
 
 This project should be run into Glitch!
+
+### My Glitch
+
+https://glitch.com/@wendersonpdas
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ const timestampRouter = require('./timestamp');
 
 router.use('/timestamp', timestampRouter);
 
+router.get('/', (req, res) => {
+  // const IP =
+  //   req.headers['x-forwarded-for'].split(', ').pop() ||
+  //   req.connection.remoteAddress;
+  const languages = req.acceptsLanguages();
+  const userAgent = req.get('user-agent');
+
+  res.json({
+    // ipaddress: IP,
+    language: languages.join(','),
+    software: userAgent
+  });
+});
+
 module.exports = router;
